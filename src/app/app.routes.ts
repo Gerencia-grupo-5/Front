1c2ee194f95c08bd59ab49b579ed64b7de03bd0f
USER <simonortega@outlook.com>
2025-04-30 19:11:00 -0500

    Creacion de componentes inicio, lista-productos y lista ingredientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,33 @@
 import { Routes } from '@angular/router';
 import { ProductTraceComponent } from './product-trace/product-trace.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { ListaIngredientesComponent } from './lista-ingredientes/lista-ingredientes.component';
 
 export const routes: Routes = [
     {
         path: '',
+        component: InicioComponent,
+        title: 'Inicio',
+    },
+    {
+        path: 'inicio',
+        component: InicioComponent,
+        title: 'Inicio',
+    },
+    {
+        path: 'product-trace',
         component: ProductTraceComponent,
         title: 'Product Trace',
+    },
+    {
+        path: 'lista-productos',
+        component: ListaProductosComponent,
+        title: 'Lista de Productos',
+    },
+    {
+        path: 'lista-ingredientes',
+        component: ListaIngredientesComponent,
+        title: 'Lista de Ingredientes',
     }
 ];
